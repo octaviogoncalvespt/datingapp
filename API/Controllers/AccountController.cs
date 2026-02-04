befdfc27b4bf0dcb9fd4e16f76b3bef0b977e3d0
octaviogoncalvespt <octaviogoncalvespt@gmail.com>
2026-02-04 18:41:50 +0000

    push to pull

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+    [HttpPost("register")] // api/account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
+    {
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email taken");
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+}
