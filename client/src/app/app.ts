f641abb0e33ab8b4240863bfb5aa5f70589639ee
octaviogoncalvespt <octaviogoncalvespt@gmail.com>
2026-02-10 23:12:50 +0000

    push form fixo

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,22 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
+import { Nav } from '../layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
+  protected router = inject(Router);
   private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'Dating app';
-  protected members = signal<User[]>([])
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
@@ -32,11 +33,12 @@ export class App implements OnInit {
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+      return lastValueFrom(
+        this.http.get<User[]>('https://localhost:5001/api/members'),
+      );
     } catch (error) {
       console.log(error);
       throw error;
     }
   }
-
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,28 +1,31 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
-  styleUrl: './nav.css'
+  styleUrl: './nav.css',
 })
 export class Nav {
-  protected accountService = inject(AccountService)
-  protected creds: any = {}
+  protected accountService = inject(AccountService);
+  private router = inject(Router);
+  protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => {
-        console.log(result);
+      next: (result) => {
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message)
-    })
+      error: (error) => alert(error.message),
+    });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
